8abbcacc0b8a912cf10514c4f57a6c2b135f259a
Moshe Gutman <moshe@geosafe.com>
2021-04-06 16:42:07 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@
 // software distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 
-// swift-tools-version:3.1
+// swift-tools-version:5.3
 
 import PackageDescription
 
