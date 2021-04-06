ea9af964052b22ceeccdf71cda306a115519bbcb
Moshe Gutman <moshe@geosafe.com>
2021-04-06 17:18:43 -0500

    Update LOKAlignment.swift

--- Sources/ObjCSupport/LOKAlignment.swift
+++ Sources/ObjCSupport/LOKAlignment.swift
@@ -11,33 +11,33 @@ import CoreGraphics
 /**
  Specifies how a layout positions itself inside of the rect that it is given to it by its parent during arrangement.
  */
-@objc open class LOKAlignment: NSObject {
+ open class LOKAlignment: NSObject {
     let alignment: Alignment
     init(alignment: Alignment) {
         self.alignment = alignment
     }
-    @objc public static let center = LOKAlignment(alignment: .center)
-    @objc public static let fill = LOKAlignment(alignment: .fill)
-    @objc public static let topCenter = LOKAlignment(alignment: .topCenter)
-    @objc public static let topFill = LOKAlignment(alignment: .topFill)
-    @objc public static let topLeading = LOKAlignment(alignment: .topLeading)
-    @objc public static let topTrailing = LOKAlignment(alignment: .topTrailing)
-    @objc public static let bottomCenter = LOKAlignment(alignment: .bottomCenter)
-    @objc public static let bottomFill = LOKAlignment(alignment: .bottomFill)
-    @objc public static let bottomLeading = LOKAlignment(alignment: .bottomLeading)
-    @objc public static let bottomTrailing = LOKAlignment(alignment: .bottomTrailing)
-    @objc public static let centerFill = LOKAlignment(alignment: Alignment(vertical: .center, horizontal: .fill))
-    @objc public static let centerLeading = LOKAlignment(alignment: .centerLeading)
-    @objc public static let centerTrailing = LOKAlignment(alignment: .centerTrailing)
-    @objc public static let fillLeading = LOKAlignment(alignment: .fillLeading)
-    @objc public static let fillTrailing = LOKAlignment(alignment: .fillTrailing)
-    @objc public static let fillCenter = LOKAlignment(alignment: Alignment(vertical: .fill, horizontal: .center))
-    @objc public static let aspectFit = LOKAlignment(alignment: .aspectFit)
+     public static let center = LOKAlignment(alignment: .center)
+     public static let fill = LOKAlignment(alignment: .fill)
+     public static let topCenter = LOKAlignment(alignment: .topCenter)
+     public static let topFill = LOKAlignment(alignment: .topFill)
+     public static let topLeading = LOKAlignment(alignment: .topLeading)
+     public static let topTrailing = LOKAlignment(alignment: .topTrailing)
+     public static let bottomCenter = LOKAlignment(alignment: .bottomCenter)
+     public static let bottomFill = LOKAlignment(alignment: .bottomFill)
+     public static let bottomLeading = LOKAlignment(alignment: .bottomLeading)
+     public static let bottomTrailing = LOKAlignment(alignment: .bottomTrailing)
+     public static let centerFill = LOKAlignment(alignment: Alignment(vertical: .center, horizontal: .fill))
+     public static let centerLeading = LOKAlignment(alignment: .centerLeading)
+     public static let centerTrailing = LOKAlignment(alignment: .centerTrailing)
+     public static let fillLeading = LOKAlignment(alignment: .fillLeading)
+     public static let fillTrailing = LOKAlignment(alignment: .fillTrailing)
+     public static let fillCenter = LOKAlignment(alignment: Alignment(vertical: .fill, horizontal: .center))
+     public static let aspectFit = LOKAlignment(alignment: .aspectFit)
 
     /**
      Positions a rect of the given size inside the given rect using the alignment spec.
      */
-    @objc public func position(size: CGSize, in rect: CGRect) -> CGRect {
+     public func position(size: CGSize, in rect: CGRect) -> CGRect {
         return alignment.position(size: size, in: rect)
     }
 }
