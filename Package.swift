e713cec88990f39c0be69a2c859e1805203f0399
Moshe Gutman <moshe@geosafe.com>
2021-04-06 17:06:41 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
-    name: "LayoutKit"
+    name: "LayoutKit",
+    products: [
+            .library(name: "LayoutKit", targets: ["LayoutKit"])
+        ],
+    dependencies: [],
+        targets: [
+            .target(name: "LayoutKit",
+                    path: "Sources")
+        ]
 )
